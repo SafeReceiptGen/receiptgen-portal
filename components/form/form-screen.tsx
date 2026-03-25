3d59d1f9e5258b8ab27001f47223b818c0d3785c
bernoullithedev <seyiramabutiate@gmail.com>
2026-03-25 09:42:21 +0000

    update: action button in sidebar

--- components/form/form-screen.tsx
+++ components/form/form-screen.tsx
@@ -186,7 +186,7 @@ export default function ReceiptFormScreen() {
       animationDuration={0.3}
     >
       <ExpandableScreenTrigger>
-        <button className="rounded-full bg-primary px-6 py-3 font-semibold text-white shadow-[0_18px_45px_rgba(11,42,74,0.35)] ring-1 ring-black/10 transition-colors hover:bg-[#10365F]">
+        <button className="rounded-full w-full bg-primary px-6 py-3 font-semibold text-white shadow-[0_18px_45px_rgba(11,42,74,0.35)] ring-1 ring-black/10 transition-colors hover:bg-[#10365F]">
           Create a Receipt
         </button>
       </ExpandableScreenTrigger>
