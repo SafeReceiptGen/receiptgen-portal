dc6492f9ac5743e95ab72f1387c86df73d51fce6
bernoullithedev <seyiramabutiate@gmail.com>
2026-03-25 12:50:55 +0000

    fix: removed user session logs

--- lib/get-server-session.ts
+++ lib/get-server-session.ts
@@ -28,7 +28,7 @@ export async function getServerSession(): Promise<Session | null> {
     }
 
     const sessionData = await response.json();
-    console.log(sessionData);
+    // console.log(sessionData);
     return sessionData; // Returns { user, session }
   } catch (error) {
     console.error("Session fetch failed:", error);
