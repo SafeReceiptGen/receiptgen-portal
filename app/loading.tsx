8036c2d8adc365ce967a07b6e0d7a15c994f480f
bernoullithedev <seyiramabutiate@gmail.com>
2026-03-25 11:28:02 +0000

    fix: removed loading file

--- app/loading.tsx
+++
@@ -1,9 +0,0 @@
-export default function loading() {
-    return(
-        <div className="flex justify-center items-center h-64">
-            <div className="animate-spin rounded-full h-12 w-12 
-            border-b-2 border-gray-800"></div>
-            <p className="mt-2 text-gray-700">Loading...</p>
-        </div>
-    );
-}
